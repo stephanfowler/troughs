71b919443adda0510b6b8118692be4a6fc032102
Stephan Fowler <stephanfowler@gmail.com>
2014-03-17 23:46:59 +0000

    Create app.clj

---
+++ app.clj
@@ -0,0 +1,27 @@
+(defn innerItems [v]
+  (if (< (count v) 3) [] (rest (butlast v))))
+
+(defn lowestEdge [v]
+  (min (first v) (last v)))
+
+(defn isValley [v]
+  (if (< (count v) 3) false (every? #(< % (lowestEdge v)) (innerItems v))))
+
+(defn leftOfPeak [v]
+  (let [peak (apply max v)]
+    (concat (take-while #(< % peak) v) [peak])))
+
+(defn rightOfPeak [v]
+   (let [peak (apply max v)]
+    (drop-while #(< % (apply max v)) v)))
+
+(defn puddleVolume [v]
+  (if (< (count v) 3)
+    0
+    (if (isValley v)
+      (- (* (- (count v) 2) (lowestEdge v)) (reduce + (innerItems v)))
+      (+ (puddleVolume (leftOfPeak v)) (puddleVolume (rightOfPeak v))))))
+
+
+(puddleVolume [1 0 8 0 0 3])
+
